144d2f0260ccf291a4fb99a8519ef5a6ea224b4c
tho_mahs <tlizana21@alumnos.utalca.cl>
2025-10-30 22:14:27 -0300

    WIP on main

--- UtalcaMarket/app/(tabs)/_layout.tsx
+++ UtalcaMarket/app/(tabs)/_layout.tsx
@@ -15,40 +15,54 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+      }}>
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: 'Home',
+        tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        href: user ? '/(tabs)' : null,
       }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-          href: user ? '/(tabs)' : null,
-        }}
-      />
-      <Tabs.Screen
-        name="LoginScreen"
-        options={{
-          title: 'Cuenta',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-          href: !user ? '/(tabs)/LoginScreen' : null,
-        }}
-      />
-      <Tabs.Screen
-        name="MisProductos"
-        options={{
-          title: 'Mis Productos',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="grid" color={color} />,
-          href: user ? '/(tabs)/MisProductos' : null,
-        }}
-      />
-      <Tabs.Screen
-        name="messages"
-        options={{
-          title: 'Mensajes',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-          href: user ? '/(tabs)/messages' : null,
-        }}
-      />
-    </Tabs>
+    />
+    <Tabs.Screen
+      name="LoginScreen"
+      options={{
+        title: 'Cuenta',
+        tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+        href: !user ? '/(tabs)/LoginScreen' : null,
+      }}
+    />
+    <Tabs.Screen
+      name="MisProductos"
+      options={{
+        title: 'Mis Productos',
+        tabBarIcon: ({ color }) => <IconSymbol size={28} name="grid" color={color} />,
+        href: user ? '/(tabs)/MisProductos' : null,
+      }}
+    />
+    <Tabs.Screen
+      name="messages"
+      options={{
+        title: 'Mensajes',
+        tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        href: user ? '/(tabs)/messages' : null,
+      }}
+    />
+    <Tabs.Screen
+      name="explore"
+      options={{
+        title: 'Explore',
+        tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        href: user ? '/(tabs)/explore' : null,
+      }}
+    />
+    <Tabs.Screen
+      name="RegisterScreen"
+      options={{
+        title: 'Registro',
+        href: null,
+      }}
+    />
+  </Tabs>
   );
 }
